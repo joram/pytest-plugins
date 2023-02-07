5a84b98dc747b877b33ab9104244b77f24374b5f
John Oram <john.c.oram@gmail.com>
2023-02-06 19:43:30 -0800

    Update pytest_profiling.py

--- pytest-profiling/pytest_profiling.py
+++ pytest-profiling/pytest_profiling.py
@@ -20,7 +20,7 @@ def clean_filename(s):
     forbidden_chars = set(r'/?<>\:*|"')
     new_s = six.text_type("".join(c if c not in forbidden_chars and ord(c) < 127 else '_'
                                  for c in s))
-    return new_s.encode('ascii', 'ignore')
+    return str(new_s.encode('ascii', 'ignore'))
 
 
 class Profiling(object):
